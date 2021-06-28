929225365266ac6615a213da825b154ba341ab98
pietro-alberti <86029229+pietro-alberti@users.noreply.github.com>
2021-06-28 17:04:17 -0300

    correção dos comentários

--- Projects/asm.s
+++ Projects/asm.s
@@ -10,19 +10,19 @@
         
 __iar_program_start
         
-main    MOV R0, #0x55       ;; atribui o valor 0x55(0000,0055) na variável R0
-        MOV R1, R0, LSL #16 ;; move logicamente 0x55 16 bits para a esquerda no R0 (0055,0000) e em seguida atribui R0 a R1
-        MOV R2, R1, LSR #8  ;; move logicamente 0x55 8 bits do R1 para a direita (0000,5500) e em seguida atribui em R2
-        MOV R3, R2, ASR #4  ;; move aritmeticamente 0x55 4 bits do R2 para a direita (0000,0550) e em seguida atribui a R3
-        MOV R4, R3, ROR #2  ;; circula 2 bytes de R3 a direita e afeta a flag c quando usada s
-        MOV R5, R4, RRX     ;; circula 1 bit de R4 a direita só que toda bit a direita gera flag c
+main    MOV R0, #0x55       ;; atribui o valor 0x55(0000,0055) na variável R0 
+        MOV R1, R0, LSL #16 ;; move logicamente 0x55 16 bits para a esquerda no R0 (0000,0055) e em seguida atribui R0 a R1 (0055,0000)
+        MOV R2, R1, LSR #8  ;; move logicamente 0x55 8 bits do R1 (0055,0000) para a direita e em seguida atribui em R2 (0000,5500)
+        MOV R3, R2, ASR #4  ;; move aritmeticamente 0x55 4 bits do R2 (0000,0550) para a direita e em seguida atribui a R3 (0000,0550)
+        MOV R4, R3, ROR #2  ;; circula 2 bits de R3 (0000,0550) a direita e afeta a flag c quando usada s (0000,0154)
+        MOV R5, R4, RRX     ;; circula 1 bit de R4 (0000,0154) a direita só que toda bit a direita gera flag c (0000,00aa)
         
-        MVN R0, #0x55        ;; atribui o valor 0x55(0000,0055) na variável R0 e depois realiza função not deixando, atribuido R0 o seu oposto (not)
-        MVN R1, R0, LSL #16  ;; move logicamente 0x55 16 bits para a esquerda no R0 (0055,0000) e em seguida atribui R0 a R1 o seu oposto (not)
-        MVN R2, R1, LSR #8   ;; move logicamente 0x55 8 bits do R1 para a direita (0000,5500) e em seguida atribui em R2 o seu oposto (not)
-        MVN R3, R2, ASR #4   ;; move aritmeticamente 0x55 4 bits do R2 para a direita (0000,0550) e em seguida atribui a R3 o seu oposto (not)
-        MVN R4, R3, ROR #2   ;; circula 2 bytes de R3 a direita, e afeta a flag c quando usada s, e atribui a R4 o seu oposto (not)
-        MVN R5, R4, RRX      ;; circula 1 bit de R4 a direita, só que toda bit a direita gera flag c, e atribui a R5 o seu oposto (not)
+        MVN R0, #0x55        ;; atribui o valor 0x55(0000,0055) na variável R0 e depois realiza função not deixando, atribuido R0 o seu oposto (not) (ffff,ffaa)
+        MVN R1, R0, LSL #16  ;; move logicamente 0x55 16 bits para a esquerda no R0 (ffff,ffaa) e em seguida atribui R0 a R1 o seu oposto (not) (0055,ffff)
+        MVN R2, R1, LSR #8   ;; move logicamente 0x55 8 bits do R1 (0055,ffff) para a direita e em seguida atribui em R2 o seu oposto (not) (ffff,aa00)
+        MVN R3, R2, ASR #4   ;; move aritmeticamente 0x55 4 bits do R2 (ffff,aa00) para a direita e em seguida atribui a R3 o seu oposto (not) (0000,055f)
+        MVN R4, R3, ROR #2   ;; circula 2 bits de R3 (0000,055f) a direita, e afeta a flag c quando usada s, e atribui a R4 o seu oposto (not) (3fff,fea8)
+        MVN R5, R4, RRX      ;; circula 1 bit de R4 (3fff,fea8) a direita, só que toda bit a direita gera flag c, e atribui a R5 o seu oposto (not) (e0000,00ab)
 
         ;; Forward declaration of sections.
         SECTION CSTACK:DATA:NOROOT(3)
